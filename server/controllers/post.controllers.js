76f839042aeb38c488c23e7102b27e7e27c7ed1d
AntonioTach <antach.vihe@gmail.com>
2022-10-23 01:03:13 -0500

    Configurations and connections

---
+++ server/controllers/post.controllers.js
@@ -0,0 +1,9 @@
+export const getPosts = (req, res) => res.send([]);
+
+export const createPost = (req, res) => res.send("New Post Created");
+
+export const updatePost = (req, res) => res.send("Updating post..");
+
+export const deletePost = (req, res) => res.send("Deleting post");
+
+export const getPost = (req, res) => res.send("Getting a post");
---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import postsRoutes from "./routes/posts.routes.js";
+import { connectDB } from "./db.js";
+import {PORT} from './config.js'
+
+const app = express();
+connectDB();
+
+app.use(postsRoutes);
+
+app.listen(PORT);
+console.log("Server running in port", PORT);
