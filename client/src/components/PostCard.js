22fb1129c2234d3997586b9f1f1856358d9672a3
AntonioTach <antach.vihe@gmail.com>
2022-12-04 22:10:25 -0600

    tailwind and postcard

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,32 @@
+import toast from 'react-hot-toast'
+
+export function PostCard({ post }) {
+  const handleDelete = () => {
+    toast((t) => (
+      <div>
+        <p>Do you want to delete?</p>
+        <div>
+          <button className='bg-red-400 hover:bg-red-600 px-3 py-2 text-white mx-2'>Delete</button>
+          <button className='bg-slate-400 hover:bg-slate-500 px-3 py-2 text-white rounded-sm mx-2'
+          onClick={() => toast.dismiss(t.id)}
+          >Cancel</button>
+        </div>
+      </div>
+    ))
+  }
+  return (
+    <div className="bg-zinc-800 text-white rounded-sm shadow-md shadow-black hover:bg-zinc-700 hover:cursor-pointer">
+      <div className="px-4 py-7">
+        <div className="flex justify-between">
+          <h3>{post.title}</h3>
+          <button className="bg-red-600 text-sm px-2 py-1 rounded-sm"
+            onClick={handleDelete}
+          >
+            Delete
+          </button>
+        </div>
+        <p>{post.description}</p>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import { usePosts } from "../context/postContext";
 import {VscEmptyWindow} from 'react-icons/vsc'
 import {Link} from 'react-router-dom'
+import {PostCard} from '../components/PostCard'
 
 export function HomePage() {
   const { posts } = usePosts();
@@ -17,11 +18,13 @@ export function HomePage() {
     <div className="text-white">
 
       <Link to="/new">Create New Post</Link>
-      {posts.map(post => (
-        <div key={post.id}>
-          {post.title}
-        </div>
-      ))}
+
+      <div className="grid grid-cols-3 gap-2">
+        {posts.map(post => (
+          <PostCard post={post} key={post.id} />
+        ))}
+      </div>
+      
     </div>
   );
 }
